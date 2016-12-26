b147be80b9a14706ab944eb4e3d3005610c11478
Bassel Shmali <bass3l@live.com>
2016-12-26 10:14:37 -0800

    update facts tables

--- wh_create.sql
+++ wh_create.sql
@@ -33,20 +33,33 @@ CREATE TABLE Dim_Mechanic (
 );
 
 ---- Fact Table:
-CREATE TABLE Fact_Repairs (
+CREATE TABLE Fact_Services (
   Id                   NUMBER(5) NOT NULL PRIMARY KEY,
-  ---- Measurse:
-  Total_Hours          NUMBER(5) NOT NULL, -- total hours spent on the repair
-  Total_Rate           NUMBER(5) NOT NULL, -- total rate of the repair
-  Total_Cost           NUMBER(5) NOT NULL, -- total cost of the repair
+  ---- Measures:
+  Total_Hours          NUMBER(5) NOT NULL, -- total hours spent on the service
+  Total_Cost           NUMBER(5) NOT NULL, -- total cost of the service (parts + mechanics)
+  Total_Mechanics_Cost NUMBER(5) NOT NULL, -- total cost of mechanics spent on this service
+  Total_Parts_Cost     NUMBER(5) NOT NULL, -- total cost of parts spent on this service
   
   ---- Foreigns:
   Time_Id      DATE      NOT NULL,
   Service_Id   NUMBER(5) NOT NULL,
   Car_Id       NUMBER(5) NOT NULL,
-  Mechanic_Id  NUMBER(5) NOT NULL,
   CONSTRAINT fk_fact_time     FOREIGN KEY (Time_Id)     REFERENCES Dim_Time(Time_Id),
   CONSTRAINT fk_fact_service  FOREIGN KEY (Service_id)  REFERENCES Dim_Service(Service_Id),
-  CONSTRAINT fk_fact_car      FOREIGN KEY (Car_Id)      REFERENCES Dim_Car(Car_Id),
-  CONSTRAINT fk_fact_mechanic FOREIGN KEY (Mechanic_Id) REFERENCES Dim_Mechanic(Mechanic_Id)
+  CONSTRAINT fk_fact_car      FOREIGN KEY (Car_Id)      REFERENCES Dim_Car(Car_Id)
+);
+
+CREATE TABLE Fact_Works (
+  Id            NUMBER(5) NOT NULL PRIMARY KEY,
+  
+  -- Measures:
+  Total_Hours   NUMBER(5) NOT NULL, -- total hours spent by the mechanic on the service
+  
+  -- Foreigns:
+  Service_Id    NUMBER(5) NOT NULL,
+  Mechanic_Id   NUMBER(5) NOT NULL,
+  
+  CONSTRAINT fk_fact_works_mechanic   FOREIGN KEY (Mechanic_Id) REFERENCES Dim_Mechanic(Mechanic_Id),
+  CONSTRAINT fk_fact_works_service    FOREIGN KEY (Service_Id)  REFERENCES Dim_Service(Service_Id)  
 );
