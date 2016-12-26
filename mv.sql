918ae8629a038b326f22201f0aa884d059872389
Bassel Shmali <bass3l@live.com>
2016-12-26 14:29:41 -0800

    edit mv

--- mv.sql
+++ mv.sql
@@ -2,9 +2,9 @@ CREATE MATERIALIZED VIEW mechanics_mv
 BUILD IMMEDIATE 
 REFRESH COMPLETE
 AS
-  SELECT m.name        as Mechanic_Name, 
-         s.name        as Service_Name,
-         f.total_hours as Total_Hours
+  SELECT m.first_name || ' ' || m.last_name as Mechanic_Name, 
+         s.name                             as Service_Name,
+         f.total_hours                      as Total_Hours
   FROM
          Fact_Works f, Dim_Mechanic m, Dim_Service s
   WHERE 
